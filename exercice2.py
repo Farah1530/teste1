c72177adb9fb1e2f15b592548e8e68adc4c221dd
Farah Mohamed Ahmed <ps51mdf@eduvaud.ch>
2025-12-03 11:06:19 +0100

    ajout de l exercice 2

---
+++ exercice2.py
@@ -0,0 +1,14 @@
+# 02 — Conversions de type
+
+val_float = 3.7
+val_str_num = "42"
+val_str_alpha = "abc"
+
+# Conversions
+res_int = int(val_float)        # convertit le float en int → 3
+res_float = float(val_str_num)  # convertit la chaîne "42" en float → 42.0
+res_str = str(val_float)        # convertit le float en str → "3.7"
+
+if __name__ == "__main__":
+    print("Conversions:", res_int, res_float, res_str)
+    print("Types:", type(res_int), type(res_float), type(res_str))
