215f6734cae076686a63ce8d3402b54dd9973dc0
Farah Mohamed Ahmed <ps51mdf@eduvaud.ch>
2025-12-03 10:52:04 +0100

    exercice1

---
+++ exercice1.py
@@ -0,0 +1,12 @@
+f= 2
+s= 5.5      
+nom = "Alice"  
+nums = [1, 2, 3]       
+coord = (10, 20)       
+info = {"nom": "Alice", "age": 20} 
+
+info["blavl"] = "jsp"
+
+print(info)
+
+print(f, s, nom, nums, coord, info)
